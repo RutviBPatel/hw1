5d04be9a5b02de3e7fe47d2245a3f3c1c34c6305
RutviBPatel <rutvi655@gmail.com>
2024-05-27 11:11:26 +0530

    Add files via upload

---
+++ hw1/src/hw/Area.java
@@ -0,0 +1,36 @@
+package hw;
+
+import java.util.*;
+public class Area {
+	private double length,width;
+	
+	public Area() {
+		this.length = 0;
+		this.width = 0
+				;
+		}
+	
+	public void setDim(double length,double width) {
+		this.length = length;
+		this.width = width;
+	}
+	
+	public double getArea() {
+		return length * width;
+	}
+	public static void main(String args[]) {
+		Area rectangle  = new Area();
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter length of rectangle: ");
+		double length = sc.nextDouble();
+		System.out.print("Enter width of rectangle: ");
+		double width = sc.nextDouble();
+		
+		rectangle.setDim(length, width);
+		
+		System.out.print("Area of rectangle: "+rectangle.getArea());
+		
+		sc.close();
+	}
+}
